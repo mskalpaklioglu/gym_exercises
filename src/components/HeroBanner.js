44ac5556366f7e95e1d141acabdf79a8bfd94c2b
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-07-20 04:50:34 +0300

    3

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box,Stack,Typography} from '@mui/material';
+
+const HeroBanner = () => {
+  return (
+    <Box>
+        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+            Fitness Club
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
